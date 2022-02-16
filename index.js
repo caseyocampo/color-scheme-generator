b9ee982ce610cb3b2c33f7c8eb9faad6e995d82f
Casey Ocampo <caseysoflo@gmail.com>
2022-02-16 16:10:15 -0500

    Add fetch request

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// JavaScript
+let getSchemeColorBtn = document.getElementById('getSchemeColorBtn')
+getSchemeColorBtn.addEventListener('click', getSchemeColor)
+
+function getSchemeColor() {
+    let seedColor = document.getElementById('seedColor').value.slice(1, 5)
+    let colorScheme = document.getElementById('colorScheme').value
+
+    let options = {
+        method: 'GET',
+        body: JSON.stringify({
+            seedColor: seedColor,
+            colorScheme: colorScheme,
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }
+
+    fetch(
+        `https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${colorScheme}&count=5`
+    )
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
